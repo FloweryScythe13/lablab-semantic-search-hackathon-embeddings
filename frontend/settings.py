0d5504cbc3b9a4adb9d5dcf3fb53444a828303f3
FloweryScythe13 <brichett13@gmail.com>
2022-12-24 13:58:54 -0600

    tweaking settings.py paths

--- frontend/settings.py
+++ frontend/settings.py
@@ -1,10 +1,10 @@
 from decouple import config
 
 
-DATA_PATH = config("DATA_PATH", default="../sample_data/Latest_News.json")
+DATA_PATH = config("DATA_PATH", default=Path(__file__).parent.joinpath("sample_data", "Latest_News.json"))
 
 # add API_KEY in .env file
 API_KEY = config("API_KEY")
 
 # 
-EMBEDDINGS_PATH = config("EMBEDDINGS_PATH", default="lablab-semantic-search-hackathon-embeddings/embeddings/embeddings.ann")
+EMBEDDINGS_PATH = config("EMBEDDINGS_PATH", default=Path(__file__).parent.joinpath("embeddings", "embeddings.ann"))
