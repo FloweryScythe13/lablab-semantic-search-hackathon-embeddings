6ca4c141cc6c288bbe0599a88ece4188ee59fe3d
FloweryScythe13 <brichett13@gmail.com>
2022-12-24 14:03:57 -0600

    wrap Path in str

--- frontend/settings.py
+++ frontend/settings.py
@@ -1,10 +1,10 @@
 from decouple import config
 from pathlib import Path
 
-DATA_PATH = config("DATA_PATH", default=Path(__file__).parent.joinpath("sample_data", "Latest_News.json"))
+DATA_PATH = config("DATA_PATH", default=str(Path(__file__).parent.joinpath("sample_data", "Latest_News.json")))
 
 # add API_KEY in .env file
 API_KEY = config("API_KEY")
 
 # 
-EMBEDDINGS_PATH = config("EMBEDDINGS_PATH", default=Path(__file__).parent.joinpath("embeddings", "embeddings.ann"))
+EMBEDDINGS_PATH = config("EMBEDDINGS_PATH", default=str(Path(__file__).parent.joinpath("embeddings", "embeddings.ann")))
